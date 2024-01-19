7745a8d2f46e3a2de9b43962b0e7dbb6c47e56c9
Stephan <stephanantonie@gmail.com>
2024-01-19 18:12:08 +0100

    add display, button functions, operator functions
    Added functionality that links both number and operator
    buttons to relevant functions, as well as code that
    displays pressed operators and assigns them in correct
    variables. NEXT: functionality for calculation and clearing,
    making it look nice, and making it all work together.

---
+++ script.js
@@ -0,0 +1,70 @@
+//Functions to manage calculator operations
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+//preliminary function to operate calculator
+function operate(operator) {
+    if (operator == "+") {
+        add(firstNum, secondNum);
+    } else if (operator == "-") {
+        return subtract(firstNum, secondNum);
+    } else if (operator == "*") {
+        return multiply(firstNum, secondNum);
+    } else {
+        if (operator == "/" && secondNum == 0) {
+            return "You're crashing the universe!";
+        } else {
+        return divide(firstNum, secondNum);
+        }
+    }
+}
+
+let firstSlot = true;
+let displayFirst= "";
+let displaySecond = "";
+const FIRSTSET = document.getElementById("first");
+const OPDISPLAY = document.getElementById("displayop");
+const SECONDSET = document.getElementById("second");
+
+//code for setting listeners to number buttons
+const NUMBERBUTTON = document.querySelectorAll(`#numbers button`);
+NUMBERBUTTON.forEach(item => item.addEventListener(`click`, function() {
+    if (firstSlot === true) {
+        displayFirst += this.textContent;
+        showfirst();
+    } else {
+        displaySecond += this.textContent;
+        showsecond();
+    }
+}))
+//code for displaying input
+function showfirst() {
+    FIRSTSET.textContent = displayFirst; 
+};
+function showsecond() {
+    SECONDSET.textContent = displaySecond; 
+};
+function showop() {
+    OPDISPLAY.textContent = operator; 
+}
+
+//code for handling operator
+const PRESSOP = document.querySelectorAll(`#operators button`);
+PRESSOP.forEach(item => item.addEventListener(`click`, function() {
+    operator = this.textContent;
+    firstSlot = false;
+    showop();
+}))
